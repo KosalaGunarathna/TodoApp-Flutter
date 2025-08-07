cd4bcabb0e6f0a610ee606dac1767e303abc10d8
Kosala Gunarathna <140527096+KosalaGunarathna@users.noreply.github.com>
2025-08-07 20:42:05 +0530

    maked dodo change item

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,18 @@
+// import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
+
 import 'package:flutter/material.dart';
 import 'package:todoapp/color_theam/color.dart';
 import '../widgets/todo_item.dart';
 import '../model/todo.dart';
 
-class Home extends StatelessWidget {
-  Home({super.key});
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final todoList = ToDo.todoList();
 
   @override
@@ -16,89 +23,101 @@ class Home extends StatelessWidget {
       body: Stack(
         children: [
           Container(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              child: Column(
-                children: [
-                  searchBox(),
-                  Expanded(
-                    child: ListView(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(
-                            top: 10,
-                            bottom: 20,
-                          ),
-                          child: const Text(
-                            'All ToDos',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                            ),
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                          top: 10,
+                          bottom: 20,
+                        ),
+                        child: const Text(
+                          'All ToDos',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
                           ),
                         ),
-                        for (ToDo todoo in todoList) 
-                        ToDoItem.ToDoItem(todo:todoo,),
-                      ],
-                    ),
+                      ),
+                      for (ToDo todoo in todoList)
+                        ToDoItem(
+                          todo: todoo,
+                          onToDoChanged: _handleToDoChange,
+                          onDeleteItem: (){},
+                        ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
+          ),
 
-            //addd new item
+          //addd new item
 
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Row(children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
                 Expanded(
                   child: Container(
-                  margin: const EdgeInsets.only(
-                    bottom:20 ,
-                    right:20,
-                    left: 20 ),
-                     padding: EdgeInsets.symmetric(horizontal: 10),
+                    margin:
+                        const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 10),
                     decoration: BoxDecoration(
-                      color:Colors.white,
-                      boxShadow: const [BoxShadow(color: Colors.grey,
-                      offset: Offset(0.0,0.0),
-                      blurRadius: 10.0,
-                      spreadRadius: 0.0
-                      ),],
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 10.0,
+                            spreadRadius: 0.0),
+                      ],
                       borderRadius: BorderRadius.circular(10),
                     ),
-                    child: const TextField(decoration: InputDecoration(
-                      hintText: 'Add a new todo item',
-                      border: InputBorder.none,
-                     
-                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Add a new todo item',
+                        border: InputBorder.none,
+                      ),
                     ),
                   ),
                 ),
-
                 Container(
                   margin: const EdgeInsets.only(
                     bottom: 20,
                     right: 20,
                   ),
-                  child: ElevatedButton(onPressed: (){},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,     // Background color
-                    foregroundColor: Colors.white,
-                   
-                   ),
-                   child: Text('+',style: TextStyle(
-                    fontSize: 20, 
-                   ),),
-                   ),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue, // Background color
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text(
+                      '+',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
                 ),
-                
-            
-                
-              ],),
+              ],
             ),
+          ),
         ],
       ),
     );
+  }
+
+  void _handleToDoChange(ToDo todo) {
+    setState(() {
+       todo.isDone = !todo.isDone;
+    });
+   
   }
 
   Widget searchBox() {
--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -4,7 +4,10 @@ import 'package:todoapp/model/todo.dart';
 
 class ToDoItem extends StatelessWidget {
   final ToDo todo;
-  const ToDoItem.ToDoItem({Key? key, required this.todo}) : super(key: key);
+  final onToDoChanged;
+  final onDeleteItem;
+
+  const ToDoItem({Key? key, required this.todo,required this.onDeleteItem,required this.onToDoChanged}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +18,8 @@ class ToDoItem extends StatelessWidget {
               left: 20,
             ),
             onTap: () {
-              print("click on todo items");
+              // print("click on todo items");
+              onToDoChanged(todo);
             },
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10),
@@ -24,17 +28,17 @@ class ToDoItem extends StatelessWidget {
 
             // ✅ Checkbox icon
             leading: Icon(
-              todo.isDone ?Icons.check_box : Icons.check_box_outline_blank,
+              todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
               color: Colors.blue,
             ),
 
             // ✅ text
-            title:  Text(
+            title: Text(
               todo.todoText!,
               style: TextStyle(
                 fontSize: 16,
                 color: tdBlack,
-                decoration:todo.isDone ?TextDecoration.lineThrough: null,
+                decoration: todo.isDone ? TextDecoration.lineThrough : null,
               ),
             ),
 
